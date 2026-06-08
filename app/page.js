24f788ffc627e7ea6fa076a381c7b1eeef761f27
Priyanka Ganesan <gpriyankapms@gmail.com>
2026-06-08 13:41:13 -0700

    chore: establish product architecture

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="container">
-      <h1>Simple Next.js Frontend</h1>
-      <p>Your project is ready. Start editing app/page.js.</p>
-    </main>
-  );
-}
